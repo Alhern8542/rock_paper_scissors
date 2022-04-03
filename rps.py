59bedb6161c8319cda0566fdc1d91a69cdd442ce
Al Hern <Alhern8542@gmail.com>
2022-04-02 19:55:54 -0500

    feat: Keeps track of scores

--- rps.py
+++ rps.py
@@ -32,22 +32,39 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.score1 = 0
+        self.score2 = 0
 
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Player 1: {move1}  Player 2: {move2}")
+        player1_wins = beats(move1, move2)
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
+        if move1 == move2:
+            print("It is a tie!\n")
+        elif player1_wins:
+            self.score1 += 1
+            print(f"Player 1 wins round, score: {self.score1} - {self.score2} \n")
+        else:
+            self.score2 += 1
+            print(f"Player 2 wins round, score: {self.score1} - {self.score2} \n")
 
     def play_game(self):
         print("Game start!")
-        for round in range(3):
-            print(f"Round {round}:")
+        for round in range(3):  # rounds start from 1
+            print(f"Round {round+1}:")
             self.play_round()
-        print("Game over!")
+        if self.score1 == self.score2:
+            print("The game is a tie!")
+        elif self.score1 > self.score2:
+            print("Player 1 wins the game!")
+        else:
+            print("Player 2 wins the game!")
+        print(f"Final score: {self.score1} - {self.score2}\nGame over!")
 
 
 if __name__ == '__main__':
-    game = Game(Player(), Player())
+    game = Game(RandomPlayer(), RandomPlayer())
     game.play_game()
